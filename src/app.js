a6a61d372533d68b5f93f38ba3ba2d4c3bef1e87
Jheyker13 <jheyker13@gmail.com>
2025-03-14 11:46:49 +0000

    generador de dominio terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon', 'paredes', 'partio'];
+  const dominio = ['com', 'es', 'io', 'net']
+
+  const nombreDominio = () =>{
+    const resultadosDiv = document.getElementById('resultados');
+    pronoun.forEach(pronombre => adj.forEach(adjetivo => noun.forEach(sustantivo => dominio.forEach(final => {
+      const dominioGenerado = (`${pronombre}${adjetivo}${sustantivo}.${final}`)
+     resultadosDiv.innerHTML += `<p>${dominioGenerado}</p>`
+    }
+    ))))};
+
+  nombreDominio()
+
+
 };
